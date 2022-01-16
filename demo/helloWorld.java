dd36b1861d0c81298c9546f108c934f43dec102e
lty <tianyuanliu111@126.com>
2022-01-16 19:36:24 +0800

    Hello World

--- demo/helloWorld.java
+++ demo/helloWorld.java
@@ -2,6 +2,6 @@ package demo;
 
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("Hello World!");
     }
 }
