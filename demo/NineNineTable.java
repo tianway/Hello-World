9dcd8995dbd8723ef4fbe725863ccda94509ba0f
Tianyuan Liu <tianyuanliu111@126.com>
2022-12-02 23:33:30 +0800

    java练习 九九乘法表

---
+++ demo/NineNineTable.java
@@ -0,0 +1,16 @@
+package demo;
+
+//九九乘法表
+public class NineNineTable {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < i+1; j++) {
+//                利用printf方法格式化输出
+                System.out.printf("%1$d x %2$d = %3$d ",i,j,i*j);
+                if (j == i){
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
