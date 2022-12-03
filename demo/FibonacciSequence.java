6c24002937a6b307322c92e886229e639ac5758b
Tianyuan Liu <tianyuanliu111@126.com>
2022-12-03 19:13:54 +0800

    java练习 FibonacciSequence Update

--- demo/FibonacciSequence.java
+++ demo/FibonacciSequence.java
@@ -3,25 +3,23 @@ package demo;
 //斐波那契数列
 public class FibonacciSequence {
     public static void main(String[] args) {
-        fibonacciSequence(6);
+        fibonacciSequence(3);
     }
 
     /**
      * 显示斐波那契数列的第target位数
+     *
      * @param target 所求值在斐波那契数列中的位数
      */
     public static void fibonacciSequence(int target) {
-        int result = 0;
-        if (target == 1 || target == 2) {
-            result = 1;
-        } else if (target > 2) {
-            int head = 1, end = 1;
-            for (int i = 3; i <= target; i++) {
-                result = head + end;
-                head = end;
-                end = result;
-            }
+        int result;
+        int head = 1, end = 1;
+        for (int i = 1; i < target; i++) {
+            result = head + end;
+            head = end;
+            end = result;
         }
+        result = head;
         System.out.println("the " + target + "st number in fibonacci sequence is:" + result);
     }
 }
