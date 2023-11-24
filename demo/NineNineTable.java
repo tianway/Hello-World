b9b81cff78a2aa2d324620e72cb9c29a197ec9d4
tianway <32070585+tianway@users.noreply.github.com>
2023-11-24 09:34:25 +0800

    Feature (#5)
    * java练习 NineNineTable Update
    
    * 规范类java文件命名

--- demo/NineNineTable.java
+++ demo/NineNineTable.java
@@ -6,7 +6,7 @@ public class NineNineTable {
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < i+1; j++) {
 //                利用printf方法格式化输出
-                System.out.printf("%1$d x %2$d = %3$d ",i,j,i*j);
+                System.out.printf("%1$d x %2$d = %3$d ",j,i,i*j);
                 if (j == i){
                     System.out.println();
                 }
